c52766ce1545648e59b68c0c6ac78881fed9f005
kumagai <kumagai@mizuuchi.lab.tuat.ac.jp>
2018-11-08 23:22:51 +0900

    include it and move servo

---
+++ servo.h
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <string>
+#include <iostream>
+#include <string.h>
+#include <time.h>
+#include <sys/time.h>
+#include <unistd.h>
+ 
+#include <wiringPi.h>
+ 
+#define MOTORPWM 18
+#define POW 1024
+
+class mog_servo
+{
+  public:
+    void setup();
+    void move(int angle);
+};
+
+void mog_servo::setup(){
+
+	if(wiringPiSetupGpio()==-1){
+      std::cout<<"cannot setup gpio."<<std::endl;
+  }
+//motor setting
+	pinMode(MOTORPWM,PWM_OUTPUT);
+	pwmSetMode(PWM_MODE_MS);
+	pwmSetClock(400);
+	pwmSetRange(1024);
+}
+
+void mog_servo::move(int angle){
+  pwmWrite(MOTORPWM,angle);
+  sleep(1);
+}
+ 
+ /*
+int main(int argc, char const* argv[]){
+  mog_servo mogura;
+  mogura.setup();
+ 
+	int num=0;
+	while(num<4){
+		std::cout<<num<<std::endl;
+    mogura.move(75);
+    mogura.move(115);
+		num++;
+	}
+	return 0;
+}
+*/
