6be8517b792e35bae6bdcb921ab24c4d9032d683
kumagai <kumagai@mizuuchi.lab.tuat.ac.jp>
2018-11-08 21:01:19 +0900

    add class to move actuator

--- servo.cpp
+++ servo.cpp
@@ -10,28 +10,42 @@
  
 #define MOTORPWM 18
 #define POW 1024
- 
-int main(int argc, char const* argv[])
+
+class mog_servo
 {
-  if(wiringPiSetupGpio()==-1){
-      std::cout<<"cannot setup gpio."<<std::endl;
-      return 1;
-  }
+  public:
+    void setup();
+    void move(int angle);
+};
+
+void mog_servo::setup(){
 	//motor setting
 	pinMode(MOTORPWM,PWM_OUTPUT);
 	pwmSetMode(PWM_MODE_MS);
 	pwmSetClock(400);
 	pwmSetRange(1024);
+}
+
+void mog_servo::move(int angle){
+  pwmWrite(MOTORPWM,angle);
+  sleep(1);
+}
  
+int main(int argc, char const* argv[]){
+  mog_servo mogura;
+  if(wiringPiSetupGpio()==-1){
+      std::cout<<"cannot setup gpio."<<std::endl;
+      return 1;
+  }
+
 	char command[256];
+  mogura.setup();
  
 	int num=0;
 	while(num<10){
 		std::cout<<num<<std::endl;
-		pwmWrite(MOTORPWM,24);
-    sleep(1);
-		pwmWrite(MOTORPWM,115);
-    sleep(1);
+    mogura.move(24);
+    mogura.move(115);
 		num++;
 	}
 	return 0;
