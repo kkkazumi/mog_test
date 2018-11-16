4a4fa77e12bbf77eb637708dc247019bca7c286a
kumagai <kumagai@mizuuchi.lab.tuat.ac.jp>
2018-11-16 17:37:10 +0900

    mou , kangaeru noga tsukareta

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@
 #define MOG_UP 80
 #define MOG_DOWN 120
 
-#define PHOTO_THRE 0.005
+#define PHOTO_THRE 0.025
 
 
 int main(int argc, char const* argv[]){
@@ -26,19 +26,34 @@ int main(int argc, char const* argv[]){
 	mogura.setup();
 	mog_photo.set_adc();
 
-	float volt_val;
+	float volt_val=0;
+	float before_volt=0.1;
+	int count = 0;
+	int ran=0;
 
 	while(1){
+		srand(10);
+		ran=1000;
 
 		mog_photo.read_val(out_ch0,ch0_data,3);
 		volt_val = mog_photo.get_volt(out_ch0,ch0_data);
 		std::cout<<volt_val<<std::endl;
 
-		if(volt_val<PHOTO_THRE){
-			mogura.move(MOG_DOWN);
-		}else if(volt_val>=PHOTO_THRE){
+		//if(volt_val>PHOTO_THRE){
+		if(volt_val-before_volt>0){
+		//	if(volt_val-before_volt>0){
+				mogura.move(MOG_DOWN);
+		//	}else if(count%ran==0){
+		//		mogura.move(MOG_DOWN);
+		//	}
+		}else if(volt_val<=0.02){
 			mogura.move(MOG_UP);
+		//	if(count%ran==0){
+		//		mogura.move(MOG_DOWN);
+		//	}
 		}
+		count++;
+		before_volt=volt_val;
 
 	}
 }
