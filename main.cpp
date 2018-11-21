24d9b2f6a22e8ef1ebbd9ba5e715294d7d007307
kumagai <kumagai@mizuuchi.lab.tuat.ac.jp>
2018-11-21 15:09:24 +0900

    just be updating

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 #include <time.h>
 #include <sys/time.h>
 #include <unistd.h>
- 
+
 
 #include "servo.h"
 #include "adcread.h"
@@ -12,7 +12,11 @@
 #define MOG_UP 80
 #define MOG_DOWN 120
 
-#define PHOTO_THRE 0.025
+#define PHOTO_THRE 0.4
+
+#define S_UP 1
+#define S_DOWN 0
+
 
 
 int main(int argc, char const* argv[]){
@@ -31,28 +35,46 @@ int main(int argc, char const* argv[]){
 	int count = 0;
 	int ran=0;
 
+	int flg=S_UP;
+
+	mogura.move(MOG_UP);
+
+
 	while(1){
-		srand(10);
-		ran=1000;
-
-		mog_photo.read_val(out_ch0,ch0_data,3);
-		volt_val = mog_photo.get_volt(out_ch0,ch0_data);
-		std::cout<<volt_val<<std::endl;
-
-		//if(volt_val>PHOTO_THRE){
-		if(volt_val-before_volt>0){
-		//	if(volt_val-before_volt>0){
-				mogura.move(MOG_DOWN);
-		//	}else if(count%ran==0){
-		//		mogura.move(MOG_DOWN);
-		//	}
-		}else if(volt_val<=0.02){
-			mogura.move(MOG_UP);
-		//	if(count%ran==0){
-		//		mogura.move(MOG_DOWN);
-		//	}
-		}
 		count++;
+		srand((unsigned)time(NULL));
+
+		//mog_photo.read_val(out_ch0,ch0_data,3);
+		//volt_val = mog_photo.get_volt(out_ch0,ch0_data);
+
+		if(count==100){
+				ran = rand();
+				std::cout<<ran<<std::endl;
+				count=0;
+
+				if(ran%7==0){
+					std::cout<<"................."<<ran%7<<std::endl;
+				}else{
+					std::cout<<"................."<<rand()%100<<std::endl;
+				}
+		}
+
+/*
+		if(volt_val<PHOTO_THRE && volt_val>0.02){
+				if(flg==S_UP){
+						std::cout<<"............................1"<<std::endl;
+				}
+		}else if(volt_val>PHOTO_THRE){
+				if(flg==S_UP){
+						std::cout<<"............................2"<<std::endl;
+						mogura.move(MOG_DOWN);
+						flg=S_DOWN;
+				}else if(flg==S_DOWN){
+						std::cout<<"............................3"<<std::endl;
+				}
+		}
+*/
+
 		before_volt=volt_val;
 
 	}
