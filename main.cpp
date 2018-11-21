7afcf40d32282d5fe92fa6f1305d044af2f20523
kumagai <kumagai@mizuuchi.lab.tuat.ac.jp>
2018-11-21 20:01:29 +0900

    tried to make a program which make a robot to move according to time and photo sensor

--- main.cpp
+++ main.cpp
@@ -16,8 +16,6 @@
 #define S_UP 1
 #define S_DOWN 0
 
-
-
 int main(int argc, char const* argv[]){
 	char out_ch0[] = { 0b00000110, 0b00000000, 0b00000000 };
 	char ch0_data[] = { 0x00, 0x00, 0x00 };
@@ -41,20 +39,35 @@ int main(int argc, char const* argv[]){
 	double thre_time = 3;
 
 	while(1){
+		//photo
+		mog_photo.read_val(out_ch0,ch0_data,3);
+		volt_val = mog_photo.get_volt(out_ch0,ch0_data);
+
+		//time
 		now_timer = time(NULL);
 		dt = difftime(st_timer,now_timer);
 		std::cout<<dt<<std::endl;
 
-		if(dt > thre_time && flg == S_DOWN){
-			mogra.move(MOG_UP);
-			st_timer = time(NULL);
-			flg = S_UP;
+		//
+		if(flg == S_DOWN){
+			if(dt > thre_time){
+				mogra.move(MOG_UP);
+				st_timer = time(NULL);
+				flg = S_UP;
+			}
+		}else if(flg == S_UP){
+			if(dt > thre_time){
+				mogura.move(MOG_DOWN);
+				st_timer = time(NULL);
+				flg = S_DOWN;
+			}
+			if(volt_val>PHOTO_THRE){
+				mogra.move(MOG_DOWN);
+				st_timer = time(NULL);
+				flg = S_DOWN;
+			}
 		}
-		if(dt > thre_time && flg == S_UP){
-			mogura.move(MOG_DOWN);
-			st_timer = time(NULL);
-			flg = S_DOWN;
-		}
+	}
 
 		before_volt=volt_val;
 
