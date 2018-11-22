268c035d7d239d6769448aea4426655898582ad7
kumagai <kumagai@mizuuchi.lab.tuat.ac.jp>
2018-11-22 17:53:20 +0900

    just removed some gomi

--- adcread.cpp
+++ adcread.cpp
@@ -3,20 +3,11 @@
 #include <stdlib.h>
 #include <iostream>
 
+//to get time
 #include <time.h>
 #include <sys/time.h>
-#include "adcread.h"
 
-/*
-class mog_adc
-{
-	public:
-		void set_adc();
-		float ad2dist(int ad);
-		void read_val(char* out_ch0, char* ch0_data, int val);
-		float get_volt(char* out_ch0, char* ch0_data);
-};
-*/
+#include "adcread.h"
 
 void mog_adc::set_adc(){
   if(!bcm2835_init()) printf("end");
@@ -31,18 +22,6 @@ void mog_adc::set_adc(){
 
 void mog_adc::read_val(char* out_ch0, char* ch0_data, int val){
 	bcm2835_spi_transfernb(out_ch0, ch0_data, val);
-}
-
-float mog_adc::ad2dist(int ad){
-	float D;
-	if (ad>=250){D=999;}
-	else if(ad<250){D=60;
-	if(ad<247){D=50;
-	if(ad<245){D=40;
-	if(ad<242){D=30;
-	if(ad<230){D=20;
-	if(ad<200){D=10;}}}}}}
-	return D;
 }
 
 float mog_adc::get_volt(char* out_ch0, char* ch0_data){
@@ -68,19 +47,27 @@ int main(int arc, char **argv)
 	mog_adc mog_photo;
 	mog_photo.set_adc();
 	float val;
-  
-  while(1){
 
+	//to get time
+	struct timeval stTime, nowTime;
+	time_t diffsec;
+	suseconds_t diffsub;
+	double realsec;
+
+	gettimeofday(&stTime,NULL);
+
+  while(1){
+			//get time calc dt
+			gettimeofday(&nowTime,NULL);
+			diffsec = difftime(nowTime.tv_sec, stTime.tv_sec);
+			diffsub = nowTime.tv_usec - stTime.tv_usec;
+			realsec = diffsec + diffsub*1e-6;
+
+			//get ad val
 			mog_photo.read_val(out_ch0,ch0_data,3);
-      //bcm2835_spi_transfernb(out_ch0, ch0_data, 3);
-      //printf("CH0:    %02X %02X %02X\n", ch0_data[0], ch0_data[1], ch0_data[2]);
-			//get_dist(out_ch0,ch0_data);
 			val = mog_photo.get_volt(out_ch0,ch0_data);
-			std::cout << val << std::endl;
-      //bcm2835_spi_transfernb(out_ch1, ch1_data, 3);
-      //printf("CH1:    %02X %02X %02X\n", ch1_data[0], ch1_data[1], ch1_data[2]);
-      //bcm2835_spi_transfernb(out_ch2, ch2_data, 3);
-      //printf("CH2:    %02X %02X %02X\n", ch2_data[0], ch2_data[1], ch2_data[2]);
+
+			std::cout << realsec << "," << val << std::endl;
   }
      
   bcm2835_spi_end();
