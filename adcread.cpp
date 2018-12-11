52e2cc352832f0642afad0a269856f6651adbbd6
kumagai <kumagai@mizuuchi.lab.tuat.ac.jp>
2018-12-11 18:59:19 +0900

    updated but not finished

--- adcread.cpp
+++ adcread.cpp
@@ -1,32 +1,31 @@
-#include <bcm2835.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <iostream>
 
+#include "SPIdev.h"
 #include "adcread.h"
 
-void mog_adc::set_adc(){
-  if(!bcm2835_init()) printf("end");
-         
-  bcm2835_spi_begin();
-  bcm2835_spi_setBitOrder(BCM2835_SPI_BIT_ORDER_MSBFIRST);
-  bcm2835_spi_setDataMode(BCM2835_SPI_MODE0);         
-  bcm2835_spi_setClockDivider(BCM2835_SPI_CLOCK_DIVIDER_128); 
-  bcm2835_spi_chipSelect(BCM2835_SPI_CS1);                 
-  bcm2835_spi_setChipSelectPolarity(BCM2835_SPI_CS1, LOW); 
-}
+#define START_BIT (1<<2)
+#define SINGLE_ENDED (1<<1)
+#define CH_SHIFT (6)
 
-void mog_adc::read_val(char* out_ch0, char* ch0_data, int val){
-	bcm2835_spi_transfernb(out_ch0, ch0_data, val);
-}
+float mog_adc::get_volt(unsigned char* out_ch0, unsigned char* ch0_data, int len){
+	uint8_t tx[len][3];
+	uint8_t rx[len][3];
 
-float mog_adc::get_volt(char* out_ch0, char* ch0_data){
 	unsigned int val;
 	float volt_value;
-	bcm2835_spi_transfernb(out_ch0, ch0_data, 3);
+
+	for(int i=0;i<len;i++){
+		tx[i][0] = START_BIT | SINGLE_ENDED;
+		tx[i][1] = out_ch0[i] << CH_SHIFT;
+	}
+
+	//bcm2835_spi_transfernb(out_ch0, ch0_data, 3);
+	SPIdev::transfer("/dev/spidev0.1", out_ch0, ch0_data, len);
+	printf("char = %s\n",ch0_data[1]);
 	val	=	(ch0_data[1]	&	0b00001111)<<8;
 	val	+=	ch0_data[2];
-	volt_value	=	3.3*(float)val/4095.0;
-	//printf("voltage = %f\n",(float)volt_value);
+	volt_value	=	5.0*(float)val/4095.0;
 	return volt_value;
 }
--- adcread.h
+++ adcread.h
@@ -1,11 +1,11 @@
-#include <bcm2835.h>
 #include <stdio.h>
+#include "SPIdev.h"
 
 class mog_adc
 {
 	public:
-		void set_adc();
-		float ad2dist(int ad);
-		void read_val(char* out_ch0, char* ch0_data, int val);
-		float get_volt(char* out_ch0, char* ch0_data);
+//		void set_adc();
+//		float ad2dist(int ad);
+//		void read_val(char* out_ch0, char* ch0_data, int val);
+		float get_volt(unsigned char* out_ch0, unsigned char* ch0_data, int len);
 };
