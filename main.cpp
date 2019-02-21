bf1c7bf0caab86da7650d222358260a1c667f8e1
kumagai <kumagai@mizuuchi.lab.tuat.ac.jp>
2019-02-22 01:31:54 +0900

    applied ring buffer?

--- main.cpp
+++ main.cpp
@@ -10,40 +10,30 @@
 #include "adcread.h"
 #include "MPU9250.h"
 
-//#define MOG_UP 80
-//#define MOG_DOWN 120
-
-#define PHOTO_THRE 0.4
-
-#define S_UP 1
-#define S_DOWN 0
-
+#define ABUF_SIZE 5
+#define IBUF_SIZE 9
+#define SET 20
+#define ALOGBUFSIZE (ABUF_SIZE * SET)
+#define ILOGBUFSIZE (IBUF_SIZE * SET)
 
 struct timeval stTime, nowTime;
 time_t diffsec;
 suseconds_t diffsub;
 double realsec;
 
+float adBuf[ABUF_SIZE];
+float imBuf[IBUF_SIZE];
 
-pthread_mutex_t mutex;
+float *rp_a,*rp_i, *wp_a,*wp_i;
+int r_count, wa_count, wi_count;
+
+pthread_mutex_t lock;
 
 void* servo_test(void* arg){
 	int ret;
 	ret = system("python /home/pi/prog/Adafruit_Python_PCA9685/examples/simpletest.py");
 }
 
-void* photo_test(void* arg){
-	char out_ch0[] = { 0b00000110, 0b01000000, 0b00000000 };
-	char ch0_data[] = { 0x00, 0x00, 0x00 };
-	float volt_val;
-	mog_adc mog_photo;
-
-	while(1){
-		mog_photo.read_val(out_ch0,ch0_data,3);
-		volt_val = mog_photo.get_volt(out_ch0,ch0_data);
-//		printf("photo volt%f\n",volt_val);
-	}
-}
 
 void* fsr_test(void* arg){
 	char out_ch0[] = { 0b00000110, 0b00000000, 0b00000000 };
@@ -53,27 +43,90 @@ void* fsr_test(void* arg){
 	float volt_photo;
 	float volt_fsr;
 	mog_adc mog_photo;
-	FILE *fp_ad;
-	fp_ad = fopen("ad_data_test.csv","w");
+	//FILE *fp_ad;
+	//fp_ad = fopen("ad_data_test.csv","w");
+	float data[5];
 
 	while(1){
-		gettimeofday(&stTime,NULL);
-		diffsec = difftime(nowTime.tv_sec, stTime.tv_sec);
-		diffsub = nowTime.tv_usec - stTime.tv_usec;
-		realsec = diffsec + diffsub*1e-6;
+		if(wa_count == SET){
+			wa_count = 0;
+			pthread_mutex_lock(&lock);
+			wp_a = adBuf;
+			pthread_mutex_unlock(&lock);
+		}
 
 		mog_photo.read_val(out_ch0,ch0_data,3);
 		volt_photo= mog_photo.get_volt(out_ch0,ch0_data);
 
 		mog_photo.read_val(out_ch1,ch1_data,3);
 		volt_fsr= mog_photo.get_volt(out_ch1,ch1_data);
-//		printf("fsr volt%f\n",volt_val);
 
-//		data[0] = volt_photo;
-//		data[1] = volt_fsr;
-		fprintf(fp_ad,"%f,%f,%f\n",realsec,volt_photo,volt_fsr);
+		gettimeofday(&stTime,NULL);
+
+		data[0] = volt_photo;
+		data[1] = volt_fsr;
+		data[2] = nowTime.tv_min;
+		data[3] = nowTime.tv_sec;
+		data[4] = nowTime.tv_usec;
+
+		pthread_mutex_lock(&lock);
+		memcpy(data,sizeof(float)*ABUF_SIZE);
+		wp_a+=LOGBUFSIZE;//?!
+		wa_count++;
+		pthread_mutex_unlock(&lock);
 	}
-	fclose(fp_ad);
+}
+
+//		printf("fsr volt%f\n",volt_val);
+	//fclose(fp_ad);
+	//	fprintf(fp_ad,"%f,%f,%f\n",realsec,volt_photo,volt_fsr);
+
+void *save_log(void* arg)
+{
+	int i;
+	int fdf_a,fdf_i;
+
+	char afile[50],ifile[50];
+	sprintf(afile,"log_data/ad_log_test.dat");
+	sprintf(ifile,"log_data/imu_log_test.dat");
+
+	if((fdf_a=open(afile,O_WRONLY|O_CREAT|O_TRUNC, 0755))==-1){
+		perror("cantopen\n");
+		exit(EXIT_FAILURE);
+	}
+
+	if((fdf_i=open(ifile,O_WRONLY|O_CREAT|O_TRUNC, 0755))==-1){
+		perror("cantopen\n");
+		exit(EXIT_FAILURE);
+	}
+
+	if(ra_count == 0){
+		pthread_mutex_lock(&lock);
+		rp_a = adBuf;
+		pthread_mutex_unlock(&lock);
+	}
+	if(ri_count == 0){
+		pthread_mutex_lock(&lock);
+		rp_i = imBuf;
+		pthread_mutex_unlock(&lock);
+	}
+
+	pthread_mutex_lock(&lock);
+	write(fdf_a,rp_a,sizeof(float)*LOGBUFSIZE);
+	write(fdf_i,rp_i,sizeof(float)*LOGBUFSIZE);
+
+	rp_a+=LOGBUFSIZE;
+	rp_i+=LOGBUFSIZE;
+
+	ra_count++;
+	ri_count++;
+
+	if(ra_count == SET) ra_count = 0;
+	if(ri_count == SET) ri_count = 0;
+	pthread_mutex_unlock(&lock);
+
+	close(fdf_a);
+	close(fdf_i);
 }
 
 void* imu_test(void* arg){
@@ -82,31 +135,47 @@ void* imu_test(void* arg){
 	float mx,my,mz;
 	int ref;
 
-	FILE *fp_imu;
-	fp_imu = fopen("imu_data_test.csv","w");
-
 	MPU9250 mog_mpu;
 	mog_mpu.initialize();
 	ref=mog_mpu.testConnection();
+	float data[9];
 
 	while(1){
-		gettimeofday(&stTime,NULL);
-		diffsec = difftime(nowTime.tv_sec, stTime.tv_sec);
-		diffsub = nowTime.tv_usec - stTime.tv_usec;
-		realsec = diffsec + diffsub*1e-6;
+		if(wi_count == SET){
+			pthread_mutex_lock(&lock);
+			wi_count = 0;
+			wp_i = adBuf;
+			pthread_mutex_unlock(&lock);
+		}
 
 		mog_mpu.getMotion9(&ax,&ay,&az,&gx,&gy,&gz,&mx,&my,&mz);
+		gettimeofday(&stTime,NULL);
 
-		printf("%f,%f,%f,",ax,ay,az);
-		printf("%f,%f,%f\n",gx,gy,gz);
-		printf("%f,%f,%f\n",mx,my,mz);
+		data[0] = ax;
+		data[1] = ay;
+		data[2] = az;
+		data[3] = gx;
+		data[4] = gy;
+		data[5] = gz;
+		data[6] = nowTime.tv_min;
+		data[7] = nowTime.tv_sec;
+		data[8] = nowTime.tv_usec;
 
-		fprintf(fp_imu,"%f,%f,%f,%f,%f,%f,%f,%f,%f,%f\n",
-			realsec,ax,ay,az,gx,gy,gz,mx,my,mz);
+		pthread_mutex_lock(&lock);
+		memcpy(data,sizeof(float)*IBUF_SIZE);
+		wp_i+=IBUFSIZE;//?!
+		pthread_mutex_unlock(&lock);
 	}
-	fclose(fp_imu);
-
 }
+
+//	FILE *fp_imu;
+//	fp_imu = fopen("imu_data_test.csv","w");
+
+//		fprintf(fp_imu,"%f,%f,%f,%f,%f,%f,%f,%f,%f,%f\n",
+//			realsec,ax,ay,az,gx,gy,gz,mx,my,mz);
+//	}
+
+//	fclose(fp_imu);
 
 int main(int argc, char const* argv[]){
 
@@ -119,6 +188,7 @@ int main(int argc, char const* argv[]){
 //	mogura.setup();
 	mog_photo.set_adc();
 
+	pthread_mutex_init(&lock, NULL);
 
 	float volt_val=0;
 	float before_volt=0.1;
@@ -132,8 +202,6 @@ int main(int argc, char const* argv[]){
 	double thre_time = 3;
 
 	pthread_create(&thr_sv, NULL, servo_test,NULL);
-	//pthread_create(&thr_ad, NULL, photo_test,&mog_photo);
-	//pthread_create(&thr_ad, NULL, photo_test,NULL);
 	pthread_create(&thr_imu, NULL, imu_test,NULL);
 	pthread_create(&thr_fsr, NULL, fsr_test,NULL);
 
@@ -145,37 +213,3 @@ int main(int argc, char const* argv[]){
 
 	pthread_mutex_destroy(&mutex);
 }
-
-/*
-	while(1){
-		//photo
-	//	mog_photo.read_val(out_ch0,ch0_data,3);
-	//	volt_val = mog_photo.get_volt(out_ch0,ch0_data);
-		//time
-		now_timer = time(NULL);
-		dt = difftime(now_timer,st_timer);
-//		std::cout<<volt_val<<std::endl;
-
-		//
-		if(flg == S_DOWN){
-			if(dt > thre_time){
-				mogura.move(MOG_UP);
-				st_timer = time(NULL);
-				flg = S_UP;
-			}
-		}else if(flg == S_UP){
-			if(dt > thre_time){
-				mogura.move(MOG_DOWN);
-				st_timer = time(NULL);
-				flg = S_DOWN;
-			}
-			if(volt_val>PHOTO_THRE){
-				mogura.move(MOG_DOWN);
-				st_timer = time(NULL);
-				flg = S_DOWN;
-			}
-		}
-
-		before_volt=volt_val;
-	}
-		*/
