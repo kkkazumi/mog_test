6d696972455e0cfae932b3e8570ff374be8b64a0
kumagai <kumagai@mizuuchi.lab.tuat.ac.jp>
2019-08-30 11:16:04 +0900

    got 8 data of mogura

--- main.cpp
+++ main.cpp
@@ -48,21 +48,45 @@ void* fsr_test(void* arg){
 
 	char out_ch0[] = { 0b00000110, 0b00000000, 0b00000000 };
 	char out_ch1[] = { 0b00000110, 0b01000000, 0b00000000 };
+	char out_ch2[] = { 0b00000110, 0b10000000, 0b00000000 };
+	char out_ch3[] = { 0b00000110, 0b11000000, 0b00000000 };
+	char out_ch4[] = { 0b00000111, 0b00000000, 0b00000000 };
+	char out_ch5[] = { 0b00000111, 0b01000000, 0b00000000 };
+	char out_ch6[] = { 0b00000111, 0b10000000, 0b00000000 };
+	char out_ch7[] = { 0b00000111, 0b11000000, 0b00000000 };
 	char ch0_data[] = { 0x00, 0x00, 0x00 };
 	char ch1_data[] = { 0x00, 0x00, 0x00 };
-	float volt_photo;
+	char ch2_data[] = { 0x00, 0x00, 0x00 };
+	char ch3_data[] = { 0x00, 0x00, 0x00 };
+	char ch4_data[] = { 0x00, 0x00, 0x00 };
+	char ch5_data[] = { 0x00, 0x00, 0x00 };
+	char ch6_data[] = { 0x00, 0x00, 0x00 };
+	char ch7_data[] = { 0x00, 0x00, 0x00 };
+	float volt_photo1;
+	float volt_photo2;
+	float volt_photo3;
+	float volt_photo4;
+	float volt_photo5;
+	float volt_photo6;
+	float volt_photo7;
 	float volt_fsr;
 	mog_adc mog_photo;
 	mog_photo.set_adc();
 
 	FILE *fp_ad;
 	fp_ad = fopen("ad_data_test.csv","w");
-	float data[5];
+	float data[11];
 
 	while(1){
 
-		volt_photo= mog_photo.get_volt(out_ch0,ch0_data);
-		volt_fsr= mog_photo.get_volt(out_ch1,ch1_data);
+		volt_photo1= mog_photo.get_volt(out_ch0,ch0_data);
+		volt_photo2= mog_photo.get_volt(out_ch1,ch1_data);
+		volt_photo3= mog_photo.get_volt(out_ch2,ch2_data);
+		volt_photo4= mog_photo.get_volt(out_ch3,ch3_data);
+		volt_photo5= mog_photo.get_volt(out_ch4,ch4_data);
+		volt_photo6= mog_photo.get_volt(out_ch5,ch5_data);
+		volt_photo7= mog_photo.get_volt(out_ch6,ch6_data);
+		volt_fsr= mog_photo.get_volt(out_ch7,ch7_data);
 
 		struct timeval nowTime;
 		gettimeofday(&nowTime,NULL);
@@ -70,16 +94,29 @@ void* fsr_test(void* arg){
 		t_st = localtime(&timer);
 //		printf("fsr volt%f\n",volt_val);
 
-		data[0] = volt_photo;
-		data[1] = volt_fsr;
-		data[2] = (float)(t_st->tm_min);
-		data[3] = (float)(t_st->tm_sec);
-		data[4] = (float)nowTime.tv_usec;
+		data[0] = volt_photo1;
+		data[1] = volt_photo2;
+		data[2] = volt_photo3;
+		data[3] = volt_photo4;
+		data[4] = volt_photo5;
+		data[5] = volt_photo6;
+		data[6] = volt_photo7;
+		data[7] = volt_fsr;
+		data[8] = (float)(t_st->tm_min);
+		data[9] = (float)(t_st->tm_sec);
+		data[10] = (float)nowTime.tv_usec;
 
+		printf("%f,%f,%f,%f,%f,%f,%f,%f\n",
+			data[0],data[1],data[2],data[3],
+			data[4],data[5],data[6],data[7]);
 //		data[0] = volt_photo;
 //		data[1] = volt_fsr;
-		fprintf(fp_ad,"%f,%f,%d,%d,%f,\n",
-			data[0],data[1],(int)t_st->tm_min,(int)t_st->tm_sec,data[4]);
+		fprintf(fp_ad,"%f,%f,%f,%f,%f,%f,%f,%f,%d,%d,%f,\n",
+			data[0],data[1],
+			data[2],data[3],
+			data[4],data[5],
+			data[6],data[7],
+			(int)t_st->tm_min,(int)t_st->tm_sec,data[10]);
 			//data[0],data[1],data[2],data[3],data[4]);
 	}
 	fclose(fp_ad);
@@ -123,9 +160,9 @@ void* imu_test(void* arg){
 		data[7] = (float)(t_st->tm_sec);
 		data[8] = (float)nowTime.tv_usec;
 
-		printf("%f,%f,%f,",ax,ay,az);
-		printf("%f,%f,%f\n",gx,gy,gz);
-		printf("%f,%f,%f\n",mx,my,mz);
+		//printf("%f,%f,%f,",ax,ay,az);
+		//printf("%f,%f,%f\n",gx,gy,gz);
+		//printf("%f,%f,%f\n",mx,my,mz);
 
 		fprintf(fp_imu,"%f,%f,%f,%f,%f,%f,%d,%d,%f\n",
 			data[0],data[1],data[2],data[3],data[4],data[5],
