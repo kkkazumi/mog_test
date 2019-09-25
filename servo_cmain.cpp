a331a8b0689a49b1f90c1c7cc3f5206919e4a8d8
kumagai <kumagai@mizuuchi.lab.tuat.ac.jp>
2019-09-25 23:30:46 +0900

    changed

--- servo_cmain.cpp
+++ servo_cmain.cpp
@@ -19,14 +19,12 @@ int main(int argc, char **argv)
  char *i2cFileName = "/dev/i2c-1"; // I2Cデバイスのパス（古いものはi2c-0）
  int driverAddress = 0x40;
  
- //
  if ((i2c = open(i2cFileName, O_RDWR)) < 0)
 {
   printf("Faild to open i2c port\n");
   exit(1);
  }
  
- //
  if (ioctl(i2c, I2C_SLAVE, driverAddress) < 0)
  {
   printf("Unable to get bus access to talk to slave\n");
@@ -40,45 +38,22 @@ int main(int argc, char **argv)
  
  usleep(100000);
  
- // サーボ制御パルス周波数の設定。
  servo.setPWMFreq(SERVO_CONTROL_FREQUENCY);
- 
- // サーボをセンタ位置へ。
- servo.setServoPulse(9, SERVO_CENTER_PULSE_WIDTH_US);
- //servo.setServoPulse(1, SERVO_CENTER_PULSE_WIDTH_US);
- //servo.setServoPulse(2, SERVO_CENTER_PULSE_WIDTH_US);
- //servo.setServoPulse(3, SERVO_CENTER_PULSE_WIDTH_US);
- 
  
  sleep(1);
  printf("slept\n");
- 
- // とりあえず、適当に動かしてみる。
- //servo.setServoPulse(1, (SERVO_CENTER_PULSE_WIDTH_US - SERVO_RANGE_PULSE_WIDTH_US / 4));
  
  while(true)
  {
  	printf("slept\n");
   
   servo.setServoPulse(9, 4096);
-	printf("off\n");
-  //servo.setServoPulse(3, (SERVO_CENTER_PULSE_WIDTH_US - SERVO_RANGE_PULSE_WIDTH_US / 4));
   
   usleep(1000000);
   
-  //servo.setServoPulse(1, (SERVO_CENTER_PULSE_WIDTH_US - SERVO_RANGE_PULSE_WIDTH_US / 4));
-  //servo.setServoPulse(2, (SERVO_CENTER_PULSE_WIDTH_US - SERVO_RANGE_PULSE_WIDTH_US / 4));
-  
-  
-  //servo.setServoPulse(9, (SERVO_CENTER_PULSE_WIDTH_US - SERVO_RANGE_PULSE_WIDTH_US / 4));
   servo.setServoPulse(9, 0);
-	printf("on\n");
-  //servo.setServoPulse(3, SERVO_CENTER_PULSE_WIDTH_US);
   
   usleep(1000000);
-  
-  //servo.setServoPulse(1, SERVO_CENTER_PULSE_WIDTH_US);
-  //servo.setServoPulse(2, (SERVO_CENTER_PULSE_WIDTH_US + SERVO_RANGE_PULSE_WIDTH_US / 4));
   
  }
  
